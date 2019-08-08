1e7994aad318310a821bb3f994eb8507e025ca8a
MAC <oketegah@gmail.com>
2019-08-08 15:26:58 +0100

    completed navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Login from './pages/Login';
+import Navbar from './components/Navbar';
+
 //Testing login route
 function App() {
   return (
     <div>
+      <Navbar />
       <Login />
     </div>
   );
