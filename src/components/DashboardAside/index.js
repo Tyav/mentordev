b7bab75c888081bc7a365f44d796e33c61da6ace
iKnowJavaScript <remymartins0398@yahoo.com>
2019-08-09 03:07:59 +0100

    Re-style dashboard aside

--- src/components/DashboardAside/index.js
+++ src/components/DashboardAside/index.js
@@ -3,21 +3,25 @@ import styles from './DashboardAside.module.css';
 
 const DashboardAside = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.inner_container}>
-        <div className={styles.logo}>DEVMENTOR</div>
-        <div className={styles.navigation}>
-          <div className={styles.link}>DashBoard</div>
-          <div className={styles.link}>Profile</div>
-          <div className={styles.link}>Requests</div>
-        </div>
-        <div className={styles.log_out}>
-          <a href="/" rel="noopener noreferrer" className={styles.user_icon}>
-            <div className={styles.go_out_icon}>
-              <i class="fas fa-sign-out-alt" />
-            </div>
-            <span>Go Out</span>
-          </a>
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.inner_container}>
+          <div className={styles.logo}>
+            DEV<span className={styles.logo_mentor}>MENTOR</span>
+          </div>
+          <div className={styles.navigation}>
+            <div className={styles.link}>DashBoard</div>
+            <div className={styles.link}>Profile</div>
+            <div className={styles.link}>Requests</div>
+          </div>
+          <div className={styles.log_out}>
+            <a href="/" rel="noopener noreferrer" className={styles.user_icon}>
+              <div className={styles.go_out_icon}>
+                <i class="fas fa-sign-out-alt" />
+              </div>
+              <span>Go Out</span>
+            </a>
+          </div>
         </div>
       </div>
     </div>
