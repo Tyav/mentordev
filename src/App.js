73888745155f6f23ec065c6b4509c840588f8c42
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-08-09 09:49:54 +0100

    created card profile for menttee

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import './App.css';
 
 import Login from './pages/Login';
+import Tag from './components/Tag';
+import ProfileHeader from './components/ProfileHeader';
 //Testing login route
 function App() {
+  const style = {
+    background: '#fff',
+    height: '370px',
+    borderBottom: '1px solid #efefef',
+    display: 'flex',
+    alignItems: 'center'
+  };
   return (
     <div>
-      <Login />
+      <ProfileHeader />
     </div>
   );
 }
