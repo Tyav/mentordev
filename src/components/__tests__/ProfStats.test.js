6c983acc5eccbf6012338af93e138c877067232e
Rukee Ojigbo <rukeeojigbo@gmail.com>
2019-08-09 17:01:29 +0100

    installed bs-jest-dom

---
+++ src/components/__tests__/ProfStats.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import ProfStats from '../SingleStats';
+import { exportAllDeclaration } from '@babel/types';
+
+afterEach(cleanup);
+
+describe('Profile Stats', () => {
+    it('should render the Profile Status', () => {
+        const {container, getAllByText}  = render( <ProfStats />);
+
+        exportAllDeclaration(getByText())
+        console.log(ProfStats)
+    })
+})
