88c27895541fd4bf21eec405ae86741a0fdde49d
whitehox <oketegah@gmail.com>
2019-08-11 21:40:48 +0100

    complete routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './App.css';
 
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Forgot from './pages/ForgotPassword';
+
+import About from './pages/About';
 //Testing login route
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Signup />
-    </div>
+    <Router>
+      <Route path="/" exact component={About} />
+      <Route path="/register" component={Signup} />
+      <Route path="/about" component={About} />
+      <Route path="/login" component={Login} />
+      <Route path="/forgotpassword" component={Forgot} />
+    </Router>
   );
 }
 
