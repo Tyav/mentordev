4e8570ddf3be7faf257b3d3763a85ed3c16f5ab2
whitehox <oketegah@gmail.com>
2019-08-11 21:42:13 +0100

    changed all a links to router Link

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,65 +1,70 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import InputField from '../../components/InputField';
 import Button from '../../components/Button';
 import FormHeader from '../../components/FormHeader';
 import Checkbox from '../../components/Checkbox';
+import Navbar from '../../components/Navbar';
 
 import './Signup.css';
 
 function Signup() {
-  const signUpFormHandler = (e) => {
-    e.preventDefault()
-  }
+  const signUpFormHandler = e => {
+    e.preventDefault();
+  };
   return (
-    <div id="loginForm">
-      <FormHeader title="Register" />
-      <form>
-        <InputField
-          label="Fullname"
-          type="text"
-          id="fullname"
-          placeholder="Eg. John Doe"
-          value=""
-        />
-        <InputField
-          label="Email"
-          type="email"
-          id="email"
-          placeholder="Eg. xyz@abc.com"
-          value=""
-        />
-        <InputField
-          label="Password"
-          type="password"
-          id="password"
-          placeholder=""
-          value=""
-        />
-        <InputField
-          label="Confirm Password"
-          type="password"
-          id="cpassword"
-          placeholder=""
-          value=""
-        />
-        <Checkbox
-          label="I am a mentor"
-          type="checkbox"
-          id="ismentor"
-          placeholder=""
-          value=""
-        />
-        <Button
-          className="btn-success-solid register"
-          text="Register"
-          onButtonClick={signUpFormHandler}
-        />
-        <p>
-          Already have an account? <a href="/">Login</a>
-        </p>
-      </form>
-    </div>
+    <>
+      <Navbar />
+      <div id="loginForm">
+        <FormHeader title="Register" />
+        <form>
+          <InputField
+            label="Fullname"
+            type="text"
+            id="fullname"
+            placeholder="Eg. John Doe"
+            value=""
+          />
+          <InputField
+            label="Email"
+            type="email"
+            id="email"
+            placeholder="Eg. xyz@abc.com"
+            value=""
+          />
+          <InputField
+            label="Password"
+            type="password"
+            id="password"
+            placeholder=""
+            value=""
+          />
+          <InputField
+            label="Confirm Password"
+            type="password"
+            id="cpassword"
+            placeholder=""
+            value=""
+          />
+          <Checkbox
+            label="I am a mentor"
+            type="checkbox"
+            id="ismentor"
+            placeholder=""
+            value=""
+          />
+          <Button
+            className="btn-success-solid register"
+            text="Register"
+            onButtonClick={signUpFormHandler}
+          />
+          <p>
+            Already have an account? <Link to="/login">Login</Link>
+          </p>
+        </form>
+      </div>
+    </>
   );
 }
 
