f9caff8212ee9ef1a05568c0455fe679ae646955
whitehox <oketegah@gmail.com>
2019-08-11 21:46:32 +0100

    completed page routing

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Footer.css';
+
+function Footer() {
+  return <footer id="footer" />;
+}
+
+export default Footer;
---
+++ src/pages/About/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import Navbar from '../../components/Navbar';
+import Button from '../../components/Button';
+import Footer from '../../components/Footer';
+
+import { Link } from 'react-router-dom';
+
+//Stylings
+import './About.css';
+
+function About() {
+  return (
+    <>
+      <Navbar link="/register" text="Register" />
+      <header className="aboutHeader">
+        <div className="aboutDescription">
+          <article>
+            <h1>
+              Mentor a <span>/>eveloper</span>
+            </h1>
+            <p>
+              Trainee students find and connect with Willing <br />
+              mentors for mentorship relationships.
+            </p>
+            <Link to="/register">
+              <Button className="btn-success-solid" text="Get Started" />
+            </Link>
+          </article>
+        </div>
+        <div className="aboutImg">
+          <img alt="mentor a developer" src="/assets/img/mentorDev.svg" />
+        </div>
+      </header>
+      <div className="menteeDescription">
+        <div className="aboutImg">
+          <img alt="mentor a developer" src="/assets/img/mentee.svg" />
+        </div>
+        <div className="aboutDescription">
+          <article>
+            <h1>
+              Get a <span>{`Mentor`}</span>
+            </h1>
+            <p>
+              Trainee students find and connect with Willing <br />
+              mentors for mentorship relationships.
+            </p>
+            <Link to="/register">
+              <Button className="btn-success-solid" text="Get Started" />
+            </Link>
+          </article>
+        </div>
+      </div>
+      <div className="callToAction" />
+      <Footer />
+    </>
+  );
+}
+
+export default About;
