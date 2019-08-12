f384e8a919d78dd7b72a8e0f04bd7fd2745444a3
iKnowJavaScript <remymartins0398@yahoo.com>
2019-08-12 03:29:31 +0100

    Tidy up styles in dashboard

--- src/components/DashboardMain/index.js
+++ src/components/DashboardMain/index.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-import styles from './DashboardMain.module.css';
-import SearchBar from '../SearchBar';
-import ProfileHeader from '../ProfileHeader';
 import MentorsProfileCard from '../MentorsProfileCard';
+import styles from './DashboardMain.module.css';
+import ProfileHeader from '../ProfileHeader';
+import SearchBar from '../SearchBar';
 
 const DashboardMain = () => {
   const [mentors, setMentors] = useState([]);
@@ -23,15 +23,17 @@ const DashboardMain = () => {
       });
   }
   return (
-    <div className={styles.container}>
-      <div className={styles.search_container}>
-        <SearchBar />
-      </div>
-      <div className={styles.profile_container}>
-        <ProfileHeader />
-      </div>
-      <div className={styles.mentors_container}>
-        <MentorsProfileCard mentors={mentors} />;
+    <div className={styles.outer_container}>
+      <div className={styles.container}>
+        <div className={styles.search_container}>
+          <SearchBar />
+        </div>
+        <div className={styles.profile_container}>
+          <ProfileHeader />
+        </div>
+        <div className={styles.mentors_container}>
+          <MentorsProfileCard mentors={mentors} />;
+        </div>
       </div>
     </div>
   );
