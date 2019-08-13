1cb97b2deef05c80cded76152edda663818974ca
whitehox <oketegah@gmail.com>
2019-08-13 08:20:53 +0100

    completed forgot password

--- src/pages/ForgotPassword/index.js
+++ src/pages/ForgotPassword/index.js
@@ -1,35 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 import InputField from '../../components/InputField';
 import Button from '../../components/Button';
 import FormHeader from '../../components/FormHeader';
 import Navbar from '../../components/Navbar';
+import FormAlert from '../../components/Alerts/FormAlert';
 
 //Stylings
 import './ForgotPassword.css';
 
 function ForgotPassword() {
+  const [value, setValue] = useState({
+    email: '',
+    response: '',
+    show: false,
+    type: '',
+  });
+
   const submitFormHandler = e => {
     e.preventDefault();
+    if (!value.email) {
+      setValue({
+        email: '',
+        response: 'Kindly fill in an email',
+        show: !value.show,
+        type: 'form-alert-danger',
+      });
+      return;
+    }
+    axios({
+      method: 'POST',
+      url: 'http://localhost:6060/api/v1/auth/forgot-password',
+      data: { ...value },
+    }).then(response => {
+      console.log(response.data.message);
+      if (
+        response.data.statusCode !== 200 ||
+        response.data.statusCode === 200
+      ) {
+        setValue({
+          email: '',
+          response: response.data.message,
+          show: !value.show,
+          type: 'form-alert-success',
+        });
+      }
+    });
   };
+
+  const inputChnageHandler = e => {
+    setValue({ ...value, [e.target.name]: e.target.value });
+  };
+
   return (
     <>
       <Navbar />
       <div id="loginForm">
         <FormHeader title="Forgot Password" />
-        <form>
+        {value.show ? (
+          <FormAlert type={value.type}>{value.response}</FormAlert>
+        ) : (
+          ''
+        )}
+        <form onSubmit={submitFormHandler}>
           <InputField
             label="Email"
             type="email"
             id="email"
             placeholder="Eg. xyz@abc.com"
-            value="oketega@gmail.com"
+            value={value.email}
+            change={inputChnageHandler}
           />
           <Button
             className="btn-success-solid"
             text="Send password reset email"
-            onButtonClick={submitFormHandler}
           />
           <p>
             Back to <Link to="/login">Login</Link>
