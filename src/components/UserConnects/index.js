a68b7c61ccaad311f977b6bc1129d2ae1208c81c
whitehox <oketegah@gmail.com>
2019-08-19 12:40:33 +0100

    added user dashboard heading

--- src/components/UserConnects/index.js
+++ src/components/UserConnects/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import UserLatestConnect from '../UserLatestConnects';
+import UserDashHeading from '../UserDashHeading';
 
 /**
  * This component is for showing the list of recent connections
@@ -12,36 +13,42 @@ import UserLatestConnect from '../UserLatestConnects';
  */
 function UserConnects() {
   return (
-    <div className="new-recent-mentor-list">
-      <UserLatestConnect
-        image="/assets/img/frustrated.png"
-        name="Bill Gates"
-        email="bill@microsoft.com"
-        tags={['.NET', 'C+']}
-        schedule="Mondays 5:00 AM to 3:30 PM"
+    <>
+      <UserDashHeading
+        text="Your most recent Mentors"
+        icon="checkbox-marked-circle-outline"
       />
-      <UserLatestConnect
-        image="/assets/img/profile_three.jpg"
-        name="Dan Abramov"
-        email="dan@facebook.com"
-        tags={['JavaScript', 'Node', 'ReactJS']}
-        schedule="Fridays 6:30 AM to 3:30 PM"
-      />
-      <UserLatestConnect
-        image="/assets/img/celebrate.png"
-        name="Oke Tega"
-        email="oketegah@gmail.com"
-        tags={['PHP', 'JavaScript']}
-        schedule="Fridays 1:25 AM to 5:30 AM"
-      />
-      <UserLatestConnect
-        image="/assets/img/profile_three.jpg"
-        name="Tolu Abramov"
-        email="tolu@barcoss.com"
-        tags={['JavaScript', 'Node', 'ReactJS']}
-        schedule="Tuesdays 1:30 AM to 3:30 PM"
-      />
-    </div>
+      <div className="new-recent-mentor-list">
+        <UserLatestConnect
+          image="/assets/img/frustrated.png"
+          name="Bill Gates"
+          email="bill@microsoft.com"
+          tags={['.NET', 'C+']}
+          schedule="Mondays 5:00 AM to 3:30 PM"
+        />
+        <UserLatestConnect
+          image="/assets/img/profile_three.jpg"
+          name="Dan Abramov"
+          email="dan@facebook.com"
+          tags={['JavaScript', 'Node', 'ReactJS']}
+          schedule="Fridays 6:30 AM to 3:30 PM"
+        />
+        <UserLatestConnect
+          image="/assets/img/celebrate.png"
+          name="Oke Tega"
+          email="oketegah@gmail.com"
+          tags={['PHP', 'JavaScript']}
+          schedule="Fridays 1:25 AM to 5:30 AM"
+        />
+        <UserLatestConnect
+          image="/assets/img/profile_three.jpg"
+          name="Tolu Abramov"
+          email="tolu@barcoss.com"
+          tags={['JavaScript', 'Node', 'ReactJS']}
+          schedule="Tuesdays 1:30 AM to 3:30 PM"
+        />
+      </div>
+    </>
   );
 }
 
