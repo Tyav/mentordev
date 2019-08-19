1fcd0ec9a7d727fa05c93223fd6deb91efed2172
iKnowJavaScript <remymartins0398@yahoo.com>
2019-08-19 14:48:08 +0100

    Fetch schedule from the server

--- src/components/UserSchedule/index.js
+++ src/components/UserSchedule/index.js
@@ -1,34 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 /**
  * For listing all schedules for a single mentor
  */
 
 function UserSchedule() {
+  const token = window.localStorage.getItem('token');
+  const [schedules, setSchedules] = useState({ data: [], loading: true });
+  useEffect(() => {
+    getSchedule();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`
+  };
+
+  const getSchedule = () => {
+    return axios({
+      method: 'GET',
+      url: `http://localhost:6060/api/v1/schedule`,
+      headers
+    })
+      .then(response => {
+        setSchedules(() => ({ data: response.data.payload, loading: false }));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
   return (
     <div className="new-dash-user-schedule">
       <div className="new-dash-user-profile">
         <h2>Current Schedule</h2>
-        <p className="new-dash-user-mentor-sch">
-          <span>Monday 1:25 AM to 5:30 AM</span>
-        </p>
-        <p className="new-dash-user-mentor-sch">
-          <span>Tuesday 5:25 AM to 5:30 PM</span>
-        </p>
-        <p className="new-dash-user-mentor-sch">
-          <span>Wednesday 12:25 AM to 12:30 AM</span>
-        </p>
-        <p className="new-dash-user-mentor-sch">
-          <span>Thursday 4:25 AM to 8:30 AM</span>
-        </p>
-        <p className="new-dash-user-mentor-sch">
-          <span>Fridays 3:25 AM to 7:30 PM</span>
-        </p>
-        <a className="new-dash-schedule-link" href="/">
-          <i className="mdi mdi-plus lg-green-ic" /> Schedule
-        </a>
+        {schedules.loading ? <p>loading...</p> : viewSchedule(schedules)}
       </div>
     </div>
+  );
+}
+
+function viewSchedule(schedules) {
+  if (schedules.data.length < 1) {
+    return (
+      <>
+        <p className="new-dash-user-mentor-sch" style={{ marginTop: '3rem', marginBottom: '1rem' }}>
+          <span>You currently Dont have no Schedule </span>
+        </p>
+        <a className="new-dash-schedule-link" href="/">
+          <i className="mdi mdi-plus lg-green-ic" /> Add Schedule
+        </a>
+      </>
+    );
+  }
+  return (
+    <>
+      {schedules.data.map((schedule, index) => (
+        <Link
+          to={`/dashboard/mentor/requests/${schedule._id}`}
+          className="schedule-list-items"
+          key={index}
+        >
+          <p className="new-dash-user-mentor-sch">
+            <span>
+              {schedule.day} {schedule.time.from} to {schedule.time.to}
+            </span>
+          </p>
+        </Link>
+      ))}
+      <a className="new-dash-schedule-link" href="/">
+        <i className="mdi mdi-plus lg-green-ic" /> Schedule
+      </a>
+    </>
   );
 }
 
