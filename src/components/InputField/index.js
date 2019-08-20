f93367419db65c464da0a439e93569ef37dd1544
whitehox <oketegah@gmail.com>
2019-08-20 08:23:32 +0100

    form border bottom

--- src/components/InputField/index.js
+++ src/components/InputField/index.js
@@ -15,7 +15,7 @@ function InputField(props) {
         onChange={props.change}
         onBlur={props.onBlur}
         disabled={props.disabled}
-        style={props.disabled ? {} : { borderBottom: '1px solid rgb(128, 120, 120)' }}
+        style={props.disabled ? {} : { borderBottom: '1px solid #a7a7a7' }}
       />
     </div>
   );
