2fa31e00ac13c391e4dcc92eb8ccddd9eaacb57a
whitehox <oketegah@gmail.com>
2019-08-21 00:50:48 +0100

    display schedule menu only if mentor is logged in

--- src/components/UserMenu/index.js
+++ src/components/UserMenu/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-function UserMenu() {
+function UserMenu({ validateType }) {
   return (
     <ul className="new-dash-menu">
       <NavLink exact to="/dashboard" activeClassName="new-dash-menu-active">
         <li>Dashboard</li>
       </NavLink>
-      <NavLink to="/dashboard/schedule" activeClassName="new-dash-menu-active">
-        <li>Manage Schedule</li>
-      </NavLink>
-      <NavLink to="/dashboard/request" activeClassName="new-dash-menu-active">
+      {validateType ? (
+        <NavLink
+          exact
+          to="/dashboard/schedule"
+          activeClassName="new-dash-menu-active"
+        >
+          <li>Manage Schedule</li>
+        </NavLink>
+      ) : (
+        ''
+      )}
+      <NavLink
+        exact
+        to="/dashboard/request"
+        activeClassName="new-dash-menu-active"
+      >
         <li>Manage Requests</li>
       </NavLink>
       <NavLink
