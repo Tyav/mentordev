20b60d1a50eaebcdaf0801a17f3f8cef2da0d9cf
whitehox <oketegah@gmail.com>
2019-08-21 02:46:24 +0100

    displayed mentees request in reuqest route

---
+++ src/pages/Dash/Request.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import UserDashHeading from '../../components/UserDashHeading';
+import Card from '../../components/Card';
+import SingleRequest from '../../components/SingleRequest';
+
+import jwt from 'jsonwebtoken';
+import axios from 'axios';
+
+function Request() {
+  const token = localStorage.getItem('token');
+  const { payload } = jwt.decode(token, { complete: true });
+  const { sub } = payload;
+
+  const [requests, setRequests] = useState([]);
+
+  useEffect(() => {
+    axios({
+      url: `http://localhost:6060/api/v1/request/${sub}`,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    }).then(response => {
+      response.data.payload.forEach(request => {
+        if (request.status === 'Pending') {
+          setRequests([request]);
+        }
+      });
+    });
+  }, []);
+
+  const style = {
+    width: '100%',
+    background: '#fff',
+    borderRadius: '4px',
+    border: '1px solid #e6ecf5',
+    padding: '20px',
+    marginBottom: '20px',
+  };
+  return (
+    <>
+      <UserDashHeading text="Pending Requests" icon="message-alert" />
+      <Card styles={style}>
+        {requests.map(request => {
+          return (
+            <SingleRequest
+              key={request._id}
+              status={request.status}
+              mentor={request.schedule.mentor.name}
+              scheduleDay={request.schedule.day}
+              fromTime={request.schedule.time.from}
+              toTime={request.schedule.time.to}
+              mentorTags={request.schedule.mentor.skills}
+            />
+          );
+        })}
+      </Card>
+    </>
+  );
+}
+
+export default Request;
