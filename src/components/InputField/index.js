3d16b0bc189bbaeedf9bd35595d4c584cd644d88
iKnowJavaScript <remymartins0398@yahoo.com>
2019-08-26 14:00:25 +0100

    add autocomplete and fix uncontrol component warning

--- src/components/InputField/index.js
+++ src/components/InputField/index.js
@@ -11,12 +11,13 @@ function InputField(props) {
         type={props.type}
         name={props.id}
         placeholder={props.placeholder}
-        value={props.value}
+        value={props.value || ''}
         onChange={props.change}
         onBlur={props.onBlur}
         disabled={props.disabled}
         style={props.disabled ? {} : { borderBottom: '1px solid #a7a7a7' }}
         onFocus={props.onFocus}
+        autoComplete={props.label}
       />
     </div>
   );
