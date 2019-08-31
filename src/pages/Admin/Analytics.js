79ef20c47d39c806156fd5a7c55e1c6798e740c1
whitehox <oketegah@gmail.com>
2019-08-31 11:37:53 +0100

    created component for analytics page

---
+++ src/pages/Admin/Analytics.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import AdminPageTitle from '../../components/AdminPageTitle';
+
+function Analytics() {
+  return (
+    <main id="admin-main-page">
+      <header className="admin-nav-header" />
+      <AdminPageTitle title="Mentor/Mentee Analytics"></AdminPageTitle>
+    </main>
+  );
+}
+
+export default Analytics;
