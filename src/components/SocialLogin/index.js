c3e55856d4c1cc090865c4077c93380760175d60
whitehox <oketegah@gmail.com>
2019-09-24 20:15:38 +0100

    reworked login and navbar components

--- src/components/SocialLogin/index.js
+++ src/components/SocialLogin/index.js
@@ -5,14 +5,19 @@ function SocialLogin(props) {
     <>
       <p style={{ textAlign: 'center' }}>{props.heading}</p>
       <center>
-      <div className="social-login">
-        <button onClick={()=>window.location.href = 'https://github.com/login/oauth/authorize?client_id=59a761c57c054d36a80d&scope=user'}>         
-          <i className="mdi mdi-github-circle" /> GitHub
-        </button>
-        {/* <button>
+        <div className="social-login">
+          <button
+            onClick={() =>
+              (window.location.href =
+                'https://github.com/login/oauth/authorize?client_id=59a761c57c054d36a80d&scope=user')
+            }
+          >
+            <i className="mdi mdi-github-circle" /> GitHub
+          </button>
+          {/* <button>
           <i className="mdi mdi-google google-social" /> Google
         </button> */}
-      </div>
+        </div>
       </center>
     </>
   );
