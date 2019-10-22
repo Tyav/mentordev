96fc3a4fbc9827d69e572b68a79084085605d468
whitehox <oketegah@gmail.com>
2019-10-22 08:41:10 +0100

    created a component to display mentor request

---
+++ src/pages/MentorRequest/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//Components
+import UserDashFilterFunc from '../../components/UserDashFilterFunc';
+import UserDashSingleCard from '../../components/UserDashSingleCard';
+
+function MentorRequest() {
+  return (
+    <>
+      <UserDashFilterFunc />
+      <UserDashSingleCard />
+    </>
+  );
+}
+
+export default MentorRequest;
