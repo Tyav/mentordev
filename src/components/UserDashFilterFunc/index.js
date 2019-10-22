2e18d7d1e4e3d96a5f63d71c09864766ecbf35bf
whitehox <oketegah@gmail.com>
2019-10-22 08:39:15 +0100

    created componet for filtering mentors approved or pending request

---
+++ src/components/UserDashFilterFunc/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function UserDashFilterFunc() {
+  return (
+    <div className="user-filter-area">
+      <p>
+        <img src="/assets/img/funnel.svg" />
+        Filter Results :
+      </p>
+      &nbsp;
+      <button className="active">
+        Approved <span>20</span>
+      </button>
+      <button>
+        Pending <span>5</span>
+      </button>
+      <button>
+        Rejected <span>13</span>
+      </button>
+      <button>
+        Cancelled <span>100</span>
+      </button>
+    </div>
+  );
+}
+
+export default UserDashFilterFunc;
