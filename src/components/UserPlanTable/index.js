cb00b9a3d6ace66913372e7edf18decfbbe927f1
whitehox <oketegah@gmail.com>
2019-10-30 15:15:27 +0100

    user plan table markup

--- src/components/UserPlanTable/index.js
+++ src/components/UserPlanTable/index.js
@@ -1,58 +1,59 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function UserPlanTable() {
+function UserPlanTable({ heading, isTitle, showActions, isLink, data }) {
   return (
     <div className="user-plan-table">
       <p>
-        <img src="/assets/img/planning.svg" />
-        Existing Plans
+        <img alt="planning" src="/assets/img/planning.svg" />
+        {heading}
       </p>
       <div className="user-plan-table-header">
         <div className="user-plan-table-head user-plan-table-sn">S/N</div>
-        <div className="user-plan-table-head user-plan-table-title">Title</div>
-        <div className="user-plan-table-head user-plan-table-action">
-          Actions
+        <div className="user-plan-table-head user-plan-table-title">
+          {isTitle ? 'Title' : 'Mentees'}
         </div>
+        {showActions ? (
+          <div className="user-plan-table-head user-plan-table-action">
+            Actions
+          </div>
+        ) : (
+          <div className="user-plan-table-head user-plan-table-action">
+            &nbsp;
+          </div>
+        )}
       </div>
       <div className="user-plan-table-body">
-        <div>
-          <p className="user-plan-table-sn">1</p>
-          <p className="user-plan-table-title">Mr Adeleke Mentorship plan</p>
-          <p className="user-plan-table-action">
-            <button className="user-plan-action-edit">
-              <i className="mdi mdi-pencil"></i>&nbsp;Edit
-            </button>
-            <button className="user-plan-action-delete">
-              <i className="mdi mdi-delete"></i>&nbsp;Delete
-            </button>
-          </p>
-        </div>
-        <div>
-          <p className="user-plan-table-sn">2</p>
-          <p className="user-plan-table-title">Grow in Python in 10 Days</p>
-          <p className="user-plan-table-action">
-            <button className="user-plan-action-edit">
-              <i className="mdi mdi-pencil"></i>&nbsp;Edit
-            </button>
-            <button className="user-plan-action-delete">
-              <i className="mdi mdi-delete"></i>&nbsp;Delete
-            </button>
-          </p>
-        </div>
-        <div>
-          <p className="user-plan-table-sn">3</p>
-          <p className="user-plan-table-title">
-            Javascript with Mosh Hammedani and Scott Moss
-          </p>
-          <p className="user-plan-table-action">
-            <button className="user-plan-action-edit">
-              <i className="mdi mdi-pencil"></i>&nbsp;Edit
-            </button>
-            <button className="user-plan-action-delete">
-              <i className="mdi mdi-delete"></i>&nbsp;Delete
-            </button>
-          </p>
-        </div>
+        {data.length
+          ? data.map((item, index) => {
+              return (
+                <div key={index}>
+                  <p className="user-plan-table-sn">{index + 1}</p>
+                  {!isLink ? (
+                    <p className="user-plan-table-title">{item.title}</p>
+                  ) : (
+                    <p className="user-plan-table-title">
+                      <Link to={item.id}>{item.name}</Link>
+                    </p>
+                  )}
+                  <p className="user-plan-table-action">
+                    {showActions ? (
+                      <>
+                        <button className="user-plan-action-edit">
+                          <i className="mdi mdi-pencil"></i>&nbsp;Edit
+                        </button>
+                        <button className="user-plan-action-delete">
+                          <i className="mdi mdi-delete"></i>&nbsp;Delete
+                        </button>{' '}
+                      </>
+                    ) : (
+                      ''
+                    )}
+                  </p>
+                </div>
+              );
+            })
+          : 'None currently available'}
       </div>
     </div>
   );
