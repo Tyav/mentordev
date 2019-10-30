e394987bf435cba7677b90be1371dd5d26c92e09
whitehox <oketegah@gmail.com>
2019-10-30 15:20:56 +0100

    completed idp editor toggle

--- src/pages/UserIdp/index.js
+++ src/pages/UserIdp/index.js
@@ -5,20 +5,42 @@ import './UserIdp.css';
 import UserPlanTable from '../../components/UserPlanTable';
 import UpcomingSchedule from '../../components/UpcomingSchedule';
 import IdpEditor from '../../components/IdpEditor';
+import MentorIdp from '../MentorIdp';
+import { readCookie } from '../../helper/cookie';
 
 function UserIdp() {
+  const validateType = readCookie('validateType');
+
+  const IdpEditorToggleHandler = e => {
+    e.preventDefault();
+    const editor = document.querySelector('.editor-area');
+    editor.classList.add('show');
+  };
+
   return (
     <>
-      <div className="editor-area">
-        <IdpEditor />
-      </div>
-      <div className="user-filter-area">
-        <button>+ Create New Plan</button>
-      </div>
-      <div className="user-existing-plan-area">
-        <UserPlanTable />
-        <UpcomingSchedule />
-      </div>
+      {!validateType ? (
+        <>
+          <div className="editor-area">
+            <IdpEditor />
+          </div>
+          <div className="user-filter-area">
+            <button onClick={IdpEditorToggleHandler}>+ Create New Plan</button>
+          </div>
+          <div className="user-existing-plan-area">
+            <UserPlanTable
+              heading="Existing Plans"
+              isTitle={true}
+              showActions={true}
+              data={[{ title: 'Mr Adeleke Mentorship Plan' }]}
+              isLink={false}
+            />
+            <UpcomingSchedule />
+          </div>
+        </>
+      ) : (
+        <MentorIdp />
+      )}
     </>
   );
 }
