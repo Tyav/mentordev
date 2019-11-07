87722d2e07c63dee546913cc8e0735a36e85a547
whitehox <oketegah@gmail.com>
2019-11-07 02:00:20 +0100

    update dashboardhomepage to display modal for a signle user

--- src/components/DashHomeList/index.js
+++ src/components/DashHomeList/index.js
@@ -1,69 +1,81 @@
-import React from 'react';
+import React, { useState, useEffect, Suspense } from 'react';
+import axios from 'axios';
 
 import UserDashSingleCard from '../UserDashSingleCard';
+import { sendGetRequest } from '../../actions';
+
+const MentorModal = React.lazy(() => import('../UserMentorDialog'));
 
 function DashHomeList() {
+  const [mentors, setMentors] = useState([]);
+  const [openDialog, setOpenDialog] = useState({
+    isOpen: false,
+    id: '',
+  });
+  const [mentorData, setMentorData] = useState();
+
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: `${process.env.REACT_APP_BACKEND_URL}/user/mentors`,
+    }).then(res => {
+      setMentors(res.data.payload);
+      console.log(res.data.payload);
+    });
+  }, []);
+
+  function openDialogHandler(e) {
+    e.preventDefault();
+    setOpenDialog({ ...openDialog, isOpen: true, id: e.target.id });
+  }
+
+  useEffect(() => {
+    if (openDialog.id) {
+      sendGetRequest(`/user/${openDialog.id}`).then(res => {
+        setMentorData(res.data.payload);
+      });
+    }
+  }, [openDialog]);
+
   return (
-    <section className="user-dash-users-list">
-      <p className="user-section-heading">
-        <img src="/assets/img/hiring.svg" /> Available Mentors
-        <div>
-          <input
-            type="text"
-            name="user-dash-search"
-            placeholder="Search Mentors by Stack, Location or Name"
-          />
-          <img alt="search" src="/assets/img/icons8-search.svg" />
+    <>
+      {openDialog.isOpen ? (
+        <Suspense fallback={<h1>Loading</h1>}>
+          <MentorModal data={mentorData} />
+        </Suspense>
+      ) : null}
+      <section className="user-dash-users-list">
+        <p className="user-section-heading">
+          <img src="/assets/img/hiring.svg" /> Available Mentors
+          <div>
+            <input
+              type="text"
+              name="user-dash-search"
+              placeholder="Search Mentors by Stack, Location or Name"
+            />
+            <img alt="search" src="/assets/img/icons8-search.svg" />
+          </div>
+        </p>
+        <div className="users-listing">
+          {mentors.map(mentor => {
+            return (
+              <UserDashSingleCard
+                key={mentor.id}
+                username={mentor.name}
+                briefDescription={mentor.bio}
+                email={mentor.email}
+                location={mentor.location}
+                stacks={mentor.skills}
+                coverImage={mentor.avatar}
+                id={mentor.id}
+                viewMore={openDialogHandler}
+                profileImage={mentor.avatar}
+              />
+            );
+          })}
         </div>
-      </p>
-      <div className="users-listing">
-        <UserDashSingleCard
-          username="OKE TEGA"
-          briefDescription="I am an aspiring Sofware Engineer from Lagos, Nigeria"
-          email="oketegah@gmail.com"
-          location="Lagos, Nigeria"
-          stacks={['PHP', 'Javascript']}
-          coverImage="https://images.unsplash.com/photo-1472437774355-71ab6752b434?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1567&amp;q=80"
-          profileImage="https://images.unsplash.com/photo-1472437774355-71ab6752b434?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1567&amp;q=80"
-        />
-        <UserDashSingleCard
-          username="Tolu Adesina"
-          briefDescription="I am an aspiring Sofware Engineer from Lagos, Nigeria"
-          email="oketegah@gmail.com"
-          location="Lagos, Nigeria"
-          stacks={['PHP', 'Javascript']}
-          coverImage="https://images.unsplash.com/photo-1561821277-481f9c1d229c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
-          profileImage="https://images.unsplash.com/photo-1472437774355-71ab6752b434?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1567&amp;q=80"
-        />
-        <UserDashSingleCard
-          username="Ibrahim Joseph"
-          briefDescription="I am an aspiring Sofware Engineer from Lagos, Nigeria"
-          email="oketegah@gmail.com"
-          location="Lagos, Nigeria"
-          stacks={['PHP', 'Javascript']}
-          coverImage="https://images.unsplash.com/photo-1472437774355-71ab6752b434?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1567&amp;q=80"
-          profileImage="https://images.unsplash.com/photo-1472437774355-71ab6752b434?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1567&amp;q=80"
-        />
-        <UserDashSingleCard
-          username="Mike Adenuga"
-          briefDescription="I am an aspiring Sofware Engineer from Lagos, Nigeria"
-          email="oketegah@gmail.com"
-          location="Lagos, Nigeria"
-          stacks={['PHP', 'Javascript']}
-          coverImage="https://images.unsplash.com/photo-1472437774355-71ab6752b434?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1567&amp;q=80"
-          profileImage="https://images.unsplash.com/photo-1472437774355-71ab6752b434?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1567&amp;q=80"
-        />
-        <UserDashSingleCard
-          username="Bill Gate"
-          briefDescription="I am an aspiring Sofware Engineer from Lagos, Nigeria"
-          email="oketegah@gmail.com"
-          location="Lagos, Nigeria"
-          stacks={['PHP', 'Javascript']}
-          coverImage="https://images.unsplash.com/photo-1472437774355-71ab6752b434?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1567&amp;q=80"
-          profileImage="https://images.unsplash.com/photo-1472437774355-71ab6752b434?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1567&amp;q=80"
-        />
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
 
