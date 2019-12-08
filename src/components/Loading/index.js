935e5854084d5865361ad603cf205b89636001da
whitehox <oketegah@gmail.com>
2019-12-09 00:40:01 +0100

    created fallback component for suspense

---
+++ src/components/Loading/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Loading.css';
+
+function Loading() {
+  return (
+    <div className="loading-area">
+      <img src="/assets/img/loading.svg" />
+    </div>
+  );
+}
+
+export default Loading;
